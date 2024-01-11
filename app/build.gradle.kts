fdc74250786fc27db3eebea1d7d37746cc93f84d
Alex <alexapazamendoza@gmail.com>
2024-01-11 16:10:50 -0500

    add coroutine and lifecycle dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,10 @@ dependencies {
     //Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    //Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+
+    //Lifecycle
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
 }
