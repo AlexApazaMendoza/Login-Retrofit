a8bc68fb642921810566f77495a4b18d071a1f69
Alex <alexapazamendoza@gmail.com>
2024-01-11 15:16:57 -0500

    add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
 }
 
 android {
@@ -44,4 +45,14 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //Volley
+    implementation("com.android.volley:volley:1.2.1")
+
+    //Gson
+    implementation("com.google.code.gson:gson:2.10.1")
+
+    //Glide
+    implementation("com.github.bumptech.glide:glide:4.14.2")
+    kapt("com.github.bumptech.glide:compiler:4.14.2")
 }
